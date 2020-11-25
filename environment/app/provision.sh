abed487d5ca20e94a3eb914ccb92ffeb96535961
samturton2 <STurton@spartaglobal.com>
2020-11-25 13:48:15 +0000

    First commit with relevant empty files and folders created

---
+++ environment/app/provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+sudo apt-get install nginx -y
+
+# finally, restart the nginx service so the new config takes hold
+sudo service nginx restart
